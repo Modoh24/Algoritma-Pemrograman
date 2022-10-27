e98dacc7a81d6b6cc7b3e024a25cc71de28f3fd6
Modoh24 <lorin.mohdo@gmail.com>
2022-10-27 11:08:27 +0700

    Menambahkan Latihan
    latihan algorritma pemrograman

---
+++ coba.1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+
+    int a = 13;
+    int b = 2;
+    char c[15] = "lorina mohdo";
+
+    puts("nama");
+    puts(c);
+    printf("terdiri dari %d hurup %d kata", a, b);
